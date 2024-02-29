91e59f96c8fd2280c9e1e1184d92f7bd970989f8
Garret Premo <garretpremo@gmail.com>
2024-02-29 02:14:38 -0500

    add images

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, effect, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { MatIconButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { MatNavList } from '@angular/material/list';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatTooltip } from '@angular/material/tooltip';
 import { RouterOutlet } from '@angular/router';
 import { distinctUntilChanged, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, MatFormField, MatLabel, MatInput, MatNavList, MatSidenavContainer, MatSidenavContent, MatSidenav, MatToolbar, MatIconButton, MatIcon ],
+  imports: [ RouterOutlet, MatFormField, MatLabel, MatInput, MatNavList, MatSidenavContainer, MatSidenavContent, MatSidenav, MatToolbar, MatIconButton, MatIcon, MatTooltip, MatButton ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material-module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
 
+    return `${value}`;
+  }
 }

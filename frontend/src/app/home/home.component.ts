a5961156f99014bf256a5482f1d80f597a01c8a5
ivlexy <browningethan23@gmail.com>
2024-03-01 11:20:01 -0500

    added welcome message and image to home page...still incomplete(needs some formatting done)

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material-module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

a786b94baf9d7792c43ac3d342a39e6b7ac6e60b
Garret Premo <garretpremo@gmail.com>
2024-03-04 14:00:27 -0500

    update webhook handler

--- webhooks/main.ts
+++ webhooks/main.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import * as crypto from 'crypto';
+import { promisify } from 'node:util';
+import { exec } from 'node:child_process';
+
+const execPromise = promisify(exec);
 
 const WEBHOOK_SECRET: string = process.env.WEBHOOK_SECRET;
 
 const main = express();
 main.use(express.json());
 
-main.post('/webhook', (request, res) => {
+main.post('/webhook', async (request, res) => {
     if (!verify_signature(request)) {
         res.status(401).send("Unauthorized");
         return;
@@ -16,9 +20,20 @@ main.post('/webhook', (request, res) => {
 
     const githubEvent = request.headers['x-github-event'];
 
+    info(`Received event: ${ githubEvent }`);
+
     if (githubEvent.includes('workflow_run')) {
-        const workflow = request.body.workflow;
-        console.log(request.body);
+        const { workflow, workflow_run } = request.body;
+
+        if (workflow.name === 'Docker Image CI' && workflow_run.conclusion === 'success') {
+            info('Github image build job succeeded, pulling...');
+            await execPromise('docker pull ethanbrowning/livelyfencing:latest');
+            info('Pulled image, deploying...');
+            await execPromise('docker container restart livelyco')
+            info('Deployment Successful.');
+        }
+    } else {
+        info(`Skipped handling ${ githubEvent }`);
     }
 });
 
@@ -28,12 +43,33 @@ main.listen(port, () => {
     console.log(`Server listening on port ${ port }`);
 });
 
-const verify_signature = (req: Request) => {
+const verify_signature = (req) => {
     const signature = crypto
         .createHmac("sha256", WEBHOOK_SECRET)
         .update(JSON.stringify(req.body))
         .digest("hex");
     let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
-    let untrusted =  Buffer.from(req.headers.get("x-hub-signature-256"), 'ascii');
+    let untrusted =  Buffer.from(req.headers['x-hub-signature-256'], 'ascii');
     return crypto.timingSafeEqual(trusted, untrusted);
 };
+
+
+const info = (message: string) => {
+    const now = new Date();
+    console.log(`INFO - ${now.toLocaleDateString()} ${now.toLocaleTimeString()} - ${ message }`);
+}
+
+process.on("SIGINT", () => {
+    info("Ctrl-C was pressed, exiting!");
+    process.exit();
+});
+
+process.on("SIGTERM", () => {
+    info("SIGTERM received... exiting!");
+    process.exit();
+});
+
+process.on("SIGKILL", () => {
+    info("SIGKILL received... exiting!");
+    process.exit();
+});

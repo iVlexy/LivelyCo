730d5093731e33761977f18d651a1cdbd9d31696
ivlexy <browningethan23@gmail.com>
2024-03-04 20:36:15 -0500

    added pictures to projects page

--- frontend/src/app/projects/projects.component.ts
+++ frontend/src/app/projects/projects.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../material-module';
+import { CommonModule } from '@angular/common';
+import { ImageDisplayComponent } from './image-display/image-display.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, CommonModule, ImageDisplayComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
+  ngOnInit(): void {
+    this.getImages()
+  }
 
+  images: string[] = []
+  getImages(){
+    const testFolder = '/assets/images/';
+    const fs = require('fs');
+    fs.readdirSync(testFolder).forEach((file: any) => {
+      console.log(file);
+      this.images.push(file)
+    })
+  }
+ 
 }

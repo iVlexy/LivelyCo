eb8adddb5784982b277334fcc04a3399dc711962
Garret Premo <garretpremo@gmail.com>
2024-03-17 22:21:11 -0400

    backend endpoint returns blobs

--- server/src/routes/google-images.router.ts
+++ server/src/routes/google-images.router.ts
@@ -9,7 +9,18 @@ googleImagesRouter.get('/photos/:folderId', async (req, res) => {
 
     let result = await driveApi.files.list({
         q: `'${getGoogleDriveFolderId(folderId)}' in parents`,
-        fields: 'files(id)'
+        fields: 'files(thumbnailLink)'
     });
-    res.send(result.data.files.map(file => file.id))
+
+    const thumbnailLinks = result.data.files.map(file => file.thumbnailLink);
+
+    const thumbnails = [];
+
+    for (const thumbnailLink of thumbnailLinks) {
+        const response = await fetch(thumbnailLink);
+
+        thumbnails.push(await response.blob());
+    }
+
+    res.send(thumbnails);
 });
